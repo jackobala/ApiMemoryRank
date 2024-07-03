8e59a13929bd83392a19fb9d8f81950a04e51a68
Jackson <jacksonoliver2009@gmail.com>
2024-07-03 12:56:46 -0300

    database update

--- database.js
+++ database.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.on('connect', () => {
